a361a66b7eb502dc9bb75a87c8d2e2fa460da887
angytobio <fewfew7824@gmail.com>
2024-08-15 22:40:18 +0700

    delete mymelody

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,6 @@ function App() {
     <div>
       <img src="https://static.wikia.nocookie.net/hellokitty/images/8/8d/Sanrio_Characters_Kuromi_Image016.png/revision/latest?cb=20170404142717" alt="kuromi" />
     </div>
-    <div>
-      <img src="https://i.pinimg.com/736x/ef/8b/4c/ef8b4c7deeecfbe4ebf0690cccb22a7c.jpg" alt="my melody" />
-    </div>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
